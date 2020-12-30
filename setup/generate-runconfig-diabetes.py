988acd38f782f506b90330ce56f204039e1357c5
Keita Onabuta <zyobuden@gmail.com>
2020-12-30 22:31:02 +0900

    test

---
+++ setup/generate-runconfig-diabetes.py
@@ -0,0 +1,15 @@
+from azureml.core import RunConfiguration, ScriptRunConfig, Dataset, Workspace, Environment
+from azureml.core.runconfig import Data, DataLocation, Dataset as RunDataset
+from azureml.core.script_run_config import get_run_config_from_script_run
+
+ws = Workspace.from_config()
+conda_env = Environment.get(ws, 'diabetes-env')
+dataset = Dataset.get_by_name(ws, 'diabetesData')
+input_name = 'diabetesData'
+compute_name = 'cpuclusters'
+
+src.run_config.framework = 'python'
+src.run_config.environment = conda_env
+src.run_config.target = compute_name
+
+get_run_config_from_script_run(src).save(name='mlops-diabetes.runconfig')
